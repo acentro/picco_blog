f0caf38ae582369ee2da95ee7f6f9e956ad7ea1c
] <brandon@acentrosys.com>
2016-04-06 21:51:26 -0400

    added install generators

---
+++ lib/generators/picco_blog/install/install_generator.rb
@@ -0,0 +1,17 @@
+module SimpleBlog
+  module Generators
+    class InstallGenerator < Rails::Generators::NamedBase
+      desc "Install PiccoBlog in your application"
+      source_root File.expand_path('../templates', __FILE__)
+
+      def create_initializer
+        copy_file "initializer.rb", "config/initializers/picco_blog.rb"
+      end
+
+      def run_migrations
+        rake "picco_blog:install:migrations"
+        #rake "acts_as_taggable_on_engine:install:migrations"
+      end
+    end
+  end
+end
---
+++ lib/generators/picco_blog/install/templates/initializer.rb
@@ -0,0 +1,17 @@
+PiccoBlog.setup do |config|
+
+  # overwrite this with the user model that will be tied to each blog post
+  # defaults to User model
+
+  config.author_class = "User"
+  
+  # overwrite this with the twitter @twitter_site_name
+  # for more info please check: https://dev.twitter.com/cards/getting-started
+
+  # config.twitter_site_name = ""
+
+  # overwrite this with the facebook app id
+  # for more info please check: https://developers.facebook.com/docs/opengraph/using-objects
+
+  # config.fb_app_id = ""  
+end
