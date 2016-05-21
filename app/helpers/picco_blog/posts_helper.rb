d243abceb86b1525ba4c3f31904c85bc9b6d72c3
Brandon Bango <brandon@acentrosys.com>
2016-05-20 22:57:38 -0400

    posts excerpt attribute

--- app/helpers/picco_blog/posts_helper.rb
+++ app/helpers/picco_blog/posts_helper.rb
@@ -5,5 +5,14 @@ module PiccoBlog
       post.created_at.strftime("%m/%d/%Y")
     end
 
+    def post_preview(post)
+      return post.excerpt unless nil_or_empty(post.excerpt)
+      truncate(post.text, length: 250)
+    end
+
+    def nil_or_empty(str)
+      str.to_s.nil? || str.to_s.empty?
+    end
+
   end
 end
