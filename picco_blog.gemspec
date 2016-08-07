a975a101c3507101d027f8fb12901a42e697a5e5
Brandon Bango <brandon@acentrosys.com>
2016-08-06 20:16:49 -0400

    dependencies

--- picco_blog.gemspec
+++ picco_blog.gemspec
@@ -24,4 +24,6 @@ Gem::Specification.new do |s|
   s.add_dependency 'redcarpet'
 
   s.add_development_dependency "sqlite3"
+  s.add_development_dependency 'minitest' 
+  s.add_development_dependency 'capybara' 
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,6 +5,8 @@ require File.expand_path("../../test/dummy/config/environment.rb",  __FILE__)
 ActiveRecord::Migrator.migrations_paths = [File.expand_path("../../test/dummy/db/migrate", __FILE__)]
 ActiveRecord::Migrator.migrations_paths << File.expand_path('../../db/migrate', __FILE__)
 require "rails/test_help"
+require 'minitest/autorun'
+require "capybara/rails"
 
 # Filter out Minitest backtrace while allowing backtrace from other libraries
 # to be shown.
@@ -18,4 +20,9 @@ if ActiveSupport::TestCase.respond_to?(:fixture_path=)
   ActiveSupport::TestCase.fixture_path = File.expand_path("../fixtures", __FILE__)
   ActionDispatch::IntegrationTest.fixture_path = ActiveSupport::TestCase.fixture_path
   ActiveSupport::TestCase.fixtures :all
+end
+
+class IntegrationTest < MiniTest::Spec
+  include Capybara::DSL
+  register_spec_type(/integration$/, self)
 end
