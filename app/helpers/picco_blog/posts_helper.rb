01a844cbbc40e0b1862713ad30ac6de50cffe92c
Brandon Bango <brandon@acentrosys.com>
2016-12-18 14:46:33 -0500

    pass in a specific date time format

--- app/helpers/picco_blog/posts_helper.rb
+++ app/helpers/picco_blog/posts_helper.rb
@@ -3,8 +3,9 @@ require 'redcarpet'
 module PiccoBlog
   module PostsHelper
 
-    def created_date_display(post)
-      post.created_at.strftime("%m/%d/%Y")
+    def created_date_display(post, format="")
+      format = "%m/%d/%Y" if format.blank?
+      post.created_at.strftime(format)
     end
 
     def post_preview(post)
