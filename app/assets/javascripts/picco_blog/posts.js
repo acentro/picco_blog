631f35da9817f3c8a0de43e2e275861a5883782b
Brandon Bango <brandon@acentrosys.com>
2016-12-17 20:21:17 -0500

    simplemde editor options

--- app/assets/javascripts/picco_blog/posts.js
+++ app/assets/javascripts/picco_blog/posts.js
@@ -1,18 +1,21 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+// Select each textarea with the class picco_blog_editor and add SimpleMDE editor
 
-var simplemde = new SimpleMDE({ 
-  element: document.getElementById("editor"),
-  toolbar: [
-    "bold", "italic", "heading", "strikethrough", "|",
-    "quote", "unordered-list", "ordered-list", "|",
-    "link", "image", "table", "horizontal-rule", "|",
-    "preview", "side-by-side", "fullscreen", "|", "guide"
-  ],
-  insertTexts: {
-        horizontalRule: ["", "\n\n-----\n\n"],
-        image: ["![](http://", ")"],
-        link: ["[", "](http://)"],
-        table: ["", "\n\n| Column 1 | Column 2 | Column 3 |\n| -------- | -------- | -------- |\n| Text     | Text      | Text     |\n\n"],
-    }
+$('textarea.picco_blog_editor').each(function() {
+    var simplemde = new SimpleMDE({
+        element: this,
+        toolbar: [
+          "bold", "italic", "heading", "strikethrough", "|",
+          "quote", "unordered-list", "ordered-list", "|",
+          "link", "image", "table", "horizontal-rule", "|",
+          "preview", "side-by-side", "fullscreen", "|", "guide"
+        ],
+        insertTexts: {
+          horizontalRule: ["", "\n\n-----\n\n"],
+          image: ["![](http://", ")"],
+          link: ["[", "](http://)"],
+          table: ["", "\n\n| Column 1 | Column 2 | Column 3 |\n| -------- | -------- | -------- |\n| Text     | Text      | Text     |\n\n"],
+        },
+        status: false
+    });
+    simplemde.render(); 
 });
