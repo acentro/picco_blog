4486d614fab0bf8364b1e12e2b64fd328909f11c
Brandon Bango <brandon@acentrosys.com>
2018-04-30 09:25:46 -0400

    Adds document.ready

--- app/assets/javascripts/picco_blog/posts.js
+++ app/assets/javascripts/picco_blog/posts.js
@@ -1,27 +1,29 @@
 // Select each textarea with the class picco_blog_editor and add SimpleMDE editor
 
-$('textarea.picco_blog_editor').each(function() {
-    var simplemde = new SimpleMDE({
-        element: this,
-        toolbar: [
-          "bold", "italic", "heading", "strikethrough", "|",
-          "quote", "unordered-list", "ordered-list", "|",
-          "link", "image", "table", "horizontal-rule", "|",
-          "preview", "side-by-side", "fullscreen", "|", "guide"
-        ],
-        insertTexts: {
-          horizontalRule: ["", "\n\n-----\n\n"],
-          image: ["![](http://", ")"],
-          link: ["[", "](http://)"],
-          table: ["", "\n\n| Column 1 | Column 2 | Column 3 |\n| -------- | -------- | -------- |\n| Text     | Text      | Text     |\n\n"],
-        },
-        status: false
-    });
-    simplemde.render(); 
-});
-
-if($("#post_tag_list").length) {
-  $("#post_tag_list").tagit({
-      availableTags: available_tags
+$(document).ready(function() {
+  $('textarea.picco_blog_editor').each(function() {
+      var simplemde = new SimpleMDE({
+          element: this,
+          toolbar: [
+            "bold", "italic", "heading", "strikethrough", "|",
+            "quote", "unordered-list", "ordered-list", "|",
+            "link", "image", "table", "horizontal-rule", "|",
+            "preview", "side-by-side", "fullscreen", "|", "guide"
+          ],
+          insertTexts: {
+            horizontalRule: ["", "\n\n-----\n\n"],
+            image: ["![](http://", ")"],
+            link: ["[", "](http://)"],
+            table: ["", "\n\n| Column 1 | Column 2 | Column 3 |\n| -------- | -------- | -------- |\n| Text     | Text      | Text     |\n\n"],
+          },
+          status: false
+      });
+      simplemde.render(); 
   });
-}
+
+  if($("#post_tag_list").length) {
+    $("#post_tag_list").tagit({
+        availableTags: available_tags
+    });
+  }
+});
